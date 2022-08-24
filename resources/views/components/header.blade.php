fa134ea1eb0aa4388a46e5df6ac50402b6384029
Akmal Gibran Baihaqi <akmalbaihaqigibran@gmail.com>
2022-08-24 15:53:32 +0700

    cleaner code

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -14,8 +14,6 @@
                 <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }}</div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
-                <div class="dropdown-title">Logged in 5 min ago</div>
-                <div class="dropdown-divider"></div>
                 <a href="#" class="dropdown-item has-icon text-danger" data-toggle="modal" data-target="#modal-logout">
                     <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
